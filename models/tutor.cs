60f1c5bb768b443545d1b9e5f60806679641a4c6
LethiciaOliveira <lethiciaoliveira1502@icloud.com>
2026-05-31 14:56:26 -0300

    Add files via upload

---
+++ models/tutor.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+    public class Tutor
+    {
+        public int Id { get; set; }
+
+        public string NomeTutor { get; set; }
+
+        public string ContatoTutor { get; set; }
+    }
+}
---
+++ models/usuario.cs
@@ -0,0 +1,11 @@
+namespace Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+    }
+}
