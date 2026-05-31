2b854d22db045b56a5ae850e6be2dee3a1ec6438
LethiciaOliveira <lethiciaoliveira1502@icloud.com>
2026-05-31 14:56:12 -0300

    Create pet.cs

---
+++ models/pet.cs
@@ -0,0 +1,15 @@
+namespace Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        public string NomePet { get; set; }
+
+        public string IdadePet { get; set; }
+
+        public int TutorId { get; set; }
+
+        public int ColeiraId { get; set; }
+    }
+}
